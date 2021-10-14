6aa197ae58992ea160381fc8c906ba9c5e426e92
riphunter618 <67683339+riphunter618@users.noreply.github.com>
2021-10-14 09:44:13 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,80 @@
+# pick empty squares
+# try no's from 1 to 9
+# find one number that works
+# repeat
+# backtrack
+board = [
+    [8, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 3, 6, 0, 0, 0, 0, 0],
+    [0, 7, 0, 0, 9, 0, 2, 0, 0],
+    [0, 5, 0, 0, 0, 7, 0, 0, 0],
+    [0, 0, 0, 0, 4, 5, 7, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 3, 0],
+    [0, 0, 1, 0, 0, 0, 0, 6, 8],
+    [0, 0, 8, 5, 0, 0, 0, 1, 0],
+    [0, 9, 0, 0, 0, 0, 4, 0, 0],
+]
+
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+    for i in range(1, 10):
+        check = valid(bo, i, (row, col))
+        if check:
+            bo[row][col] = i
+            if solve(bo):
+                return True
+        bo[row][col] = 0
+    return False
+
+
+def valid(bo, num, pos):
+    # check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+    # check column
+    for i1 in range(len(bo[0])):
+        if bo[i1][pos[1]] == num and pos[0] != i1:
+            return False
+    # check 3x3 square
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    for j in range(box_y * 3, box_y * 3 + 3):
+        for j1 in range(box_x * 3, box_x * 3 + 3):
+            if bo[j][j1] == num and (j, j1) != pos:
+                return False
+    return True
+
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print('- - - - - - - - - -')
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print('|', end='')
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end='')
+
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return i, j
+    return None
+
+
+print_board(board)
+solve(board)
+print(''
+      'here is the solution to the board'
+      '')
+print_board(board)
